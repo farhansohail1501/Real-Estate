5e1cfa3de58cb1e3073199a2eefa2af292d861b1
MuhammadSohail <msohai11@my.centennialcollege.ca>
2024-04-04 19:43:02 -0400

    Hero section added

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import './Hero.css'
+import { HiLocationMarker } from 'react-icons/hi'
 
 const Hero = () => {
   return (
     <div>
-      Hero
+      <section className="hero-wrapper">
+        <div className="paddings innerWidth flexCenter hero-container">
+          {/*left side*/}
+          <div className="flexColStart hero-left">
+            <div className="hero-title">
+              <div className="circle" />
+              <h1>Find <br />Your Dream<br /> Property</h1>
+            </div>
+            <div className="flexColStart hero-des">
+              <span>Embark on a journey to your dream property with us.</span>
+              <span>Discover tailored options that match your vision and aspirations.</span>
+            </div>
+
+            <div className="flexCenter search-bar">
+              <HiLocationMarker color="blue" size={30} />
+              <input type="text" placeholder=" " />
+              <button className='button'>Search</button>
+            </div>
+          </div>
+
+          {/*right side*/}
+          <div className="flexCenter hero-right">
+            <div className="image-container">
+              <img src="./hero-image.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
