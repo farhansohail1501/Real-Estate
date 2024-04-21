2803fb27c393778609091ceb34ed8186f8587186
MuhammadSohail <msohai11@my.centennialcollege.ca>
2024-04-21 02:36:22 -0400

    Companies and Residencies added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
 import './App.css';
 
 function App() {
@@ -10,7 +12,8 @@ function App() {
         <Header />
         <Hero />
       </div>
-
+      <Companies /> 
+      <Residencies/>
     </div>
   );
 }
---
+++ client/src/components/Companies/Companies.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Companies.css'
+
+const Companies = () => {
+  return (
+    <div>
+      <section className="c-wrapper">
+        <div className="paddings innerWidth flexCenter c-container">
+            <img src="./Company1.png" alt="" />
+            <img src="./Company2.png" alt="" />
+            <img src="./Company3.png" alt="" />
+            <img src="./Company4.png" alt="" />
+
+
+        </div>
+        </section>
+    </div>
+  )
+}
+
+export default Companies
